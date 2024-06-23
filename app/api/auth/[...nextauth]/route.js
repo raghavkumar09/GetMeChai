f3a83529c77158a18db0a4e0cfaf9bed22704569
Raghav Kumar <jharaghav330@gmail.com>
2024-06-23 11:58:50 +0530

    fix code in auth page

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,23 +1,19 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import mongoose from "mongoose"
-import User from "@/app/models/User"
-import Payment from "@/app/models/Payment"
-import connectDB from "@/app/db/connectDB"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import mongoose from "mongoose";
+import User from "@/app/models/User";
+import connectDB from "@/app/db/connectDB";
 
-const authOptions = NextAuth({
-  // Configure one or more authentication providers
+const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
 
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      // connect to database moongoose
       await connectDB();
 
       if (account.provider === "github") {
@@ -33,18 +29,18 @@ const authOptions = NextAuth({
         }
         return true;
       }
+      return false;
     },
     async session({ session, user, token }) {
       await connectDB();
 
       const dbUser = await User.findOne({ email: session.user.email });
-      // console.log("dbUser", dbUser);
       if (dbUser) {
         session.user.name = dbUser.username;
       }
       return session;
     },
-  }
-});
+  },
+};
 
-export { authOptions as GET, authOptions as POST }
+export default NextAuth(authOptions);
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['cdn.pixabay.com'],
     },
+    experimental: {
+      appDir: true, // Use new app directory structure if required
+    },
   };
   
   export default nextConfig;
