8094beac94807bf297cf8c1bf54bcf632371dd99
Raghav Kumar <jharaghav330@gmail.com>
2024-06-23 12:11:43 +0530

    fix code in auth page

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
       domains: ['cdn.pixabay.com'],
-    }
+    },
+    experimental: {
+      appDir: true, // Use new app directory structure if required
+    },
   };
   
   export default nextConfig;
